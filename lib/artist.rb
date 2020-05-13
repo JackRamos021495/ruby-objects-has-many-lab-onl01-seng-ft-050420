97e840f791164f4aa5597ad303755abefc04edcb
Jackson Ramos <jackramos021495@gmail.com>
2020-05-13 19:30:50 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,7 @@
 class Artist
   attr_accessor :name
-
+@@song_count =0
+  
   def initialize(name)
     @name = name
     @songs = []
@@ -13,5 +14,12 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
+    @@song_count += 1
   end
+
+  def add_song_by_name(song_name)
+    song_name = Song.new(song_name)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
 end
